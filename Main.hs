1fb610dfe0f383f1d2110467ae156eaa1f237e2f
Crockeo <crockeo@gmail.com>
2014-05-19 17:24:51 -0400

    Split up the route functions and made a 404 page.

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@ import Hack2.Handler.SnapServer
 
 import Network.Miku.Engine
 import Network.Miku.Utils
+import Network.Miku.Type
 import Network.Miku
 
 import Control.Monad
@@ -14,10 +15,22 @@ import Air.Env hiding ((.))
 
 import Templater
 
+-- Serving the index page
+serveIndex :: MikuMonad
+serveIndex =
+  get "/" $ html =<< io (bsPage [("pageName", "index")] "templates/index.html")
+
+-- Serving the 404 page
+serve404 :: MikuMonad
+serve404 =
+  get "*" $ html =<< io (bsPage [("pageName", "404")] "templates/404.html")
+
 main :: IO ()
 main =
   run . miku $ do
-    public (Just ".") ["/*.hs", "/css/", "/js/"]
+    -- Setting up the static files
+    public (Just ".") ["/css/", "/js/"]
 
-    -- The index page
-    get "/" $ html =<< io (bsPage [("pageName", "index")] "templates/index.html")
+    -- Setting up the routes
+    serveIndex
+    serve404
