5bc3f68fea5f157a509870733d9fdac5391e4b57
Crockeo <crockeo@gmail.com>
2014-05-19 16:35:13 -0400

    Got static serving working (just had to look at the documentation)

--- Main.hs
+++ Main.hs
@@ -1,7 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Hack2.Handler.SnapServer
+import Data.ByteString.Char8 (unpack, pack)
+import Network.Miku.Type
 import Network.Miku
 
+import Templater
+
+-- Serving the index page
+serveIndexIO :: IO MikuMonad
+serveIndexIO =
+  (bsPage [("pageName", "index")] "templates/index.html") >>= return . get "/" . html
+
 main :: IO ()
-main = run . miku $ get "/" $ text "rawr"
+main = do
+  -- Recieving the routes
+  serveIndex <- serveIndexIO
+
+  index <- bsPage [("pageName", "index")] "templates/index.html"
+
+  run . miku $ do
+    public (Just ".") ["/css/", "/js/"]
+
+    serveIndex
