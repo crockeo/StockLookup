2cc6fee769b05ebb37642dc1e51740a6aab0ac40
Crockeo <crockeo@gmail.com>
2014-05-19 18:40:03 -0400

    Added serving the stock pages (more work to be done)

--- Main.hs
+++ Main.hs
@@ -3,27 +3,49 @@
 module Main where
 
 import Hack2.Handler.SnapServer
+import Hack2.Contrib.Response
+import Hack2.Contrib.Request
+
+import Control.Monad.Reader
+import Control.Monad.Trans
+import Control.Monad
 
 import Network.Miku.Engine
 import Network.Miku.Utils
 import Network.Miku.Type
 import Network.Miku
 
-import Control.Monad
-
-import Air.Env hiding ((.))
+import Debug.Trace
 
 import Templater
+
+_parameter env name =
+  lookup name $ params env
 
 -- Serving the index page
 serveIndex :: MikuMonad
 serveIndex =
-  get "/" $ html =<< io (bsPage [("pageName", "index")] "templates/index.html")
+  get "/" $ html =<< liftIO (bsPage [("pageName", "index")] "templates/index.html")
+
+-- Serving the stock page
+serveStock :: MikuMonad
+serveStock =
+  get "/stock" $ do
+    env <- ask
+
+    case lookup "scode" $ params env of
+      Nothing  -> serveNoStock
+      Just (c) ->
+        if c == ""
+          then serveNoStock
+          else serveStock c
+  where serveNoStock   = html =<< liftIO (bsPage [("pageName", "stock")                   ] "templates/nostock.html")
+        serveStock   c = html =<< liftIO (bsPage [("pageName", "stock"), ("scode", unbs c)] "templates/stock.html"  )
 
 -- Serving the 404 page
 serve404 :: MikuMonad
 serve404 =
-  get "*" $ html =<< io (bsPage [("pageName", "404")] "templates/404.html")
+  get "*" $ html =<< liftIO (bsPage [("pageName", "404")] "templates/404.html")
 
 main :: IO ()
 main =
@@ -33,4 +55,5 @@ main =
 
     -- Setting up the routes
     serveIndex
+    serveStock
     serve404
