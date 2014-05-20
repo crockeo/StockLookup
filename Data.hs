091a096e9ca0dbf9cdfe82d0735689bb3e390424
Crockeo <crockeo@gmail.com>
2014-05-20 11:33:15 -0400

    Added a function to constructHTML from CSV data.

--- Data.hs
+++ Data.hs
@@ -20,3 +20,12 @@ urlPrefix = "http://ichart.finance.yahoo.com/table.csv?s="
 -- Converting a stock code into parsed CSV
 parseCode :: String -> IO CSV
 parseCode code = parseURL $ urlPrefix ++ code
+
+-- Constructing a piece of HTML from the parseCode
+constructHTML :: CSV -> String
+constructHTML csv =
+  "<table>\n" ++ constructHTMLRaw csv ++ "</table>"
+  where constructHTMLRaw :: CSV -> String
+        constructHTMLRaw []           = ""
+        constructHTMLRaw ((x:[]):xss) = "\t<td>" ++ x ++ "</td>\n</tr>\n<tr>\n" ++ constructHTMLRaw xss
+        constructHTMLRaw ((x:xs):xss) = "\t<td>" ++ x ++ "</td>\n" ++ constructHTMLRaw (xs:xss)
